2aed7616904d1f41b7fc17b46bb3148c87450a78
lancelink <lance@bytesec.org>
2015-10-05 15:45:49 +0800

    Added error handler and button disable during posting

--- js/01-ajax.js
+++ js/01-ajax.js
@@ -2,33 +2,32 @@ $('document').ready(function () {
 	$(function() {
 	  $('form').on('submit', function (e) {
 	    var $form = $(e.target);
-	    // A bit hacky since I hard coded the submit value in that format (that value is hardcoded to bbscgi script)
+	    var submit_txt = $(this).find('input[type="submit"]').val();
+	    // A bit hacky since I hard coded the submit value in that format (that value is hardcoded in bbscgi script)
 	    var formData = $('form').serialize()+"&submit=%E6%9B%B8%E3%81%8D%E8%BE%BC%E3%82%80";
 	    e.preventDefault();
-	    var updateProgress = function(e) {
-				// Percentage loading will be added here soon
-				$('form').find('input[type="submit"]').val('Posting...');
-			};
 	    $.ajax({
 	      url: $form.attr('action'),
 	      dataType: 'text',
 	      type: 'post',
 	      contentType: 'application/x-www-form-urlencoded; charset=utf-8',
 	      data: formData,
-	      xhr: function() {
-					var xhr = $.ajaxSettings.xhr();
-					if(xhr.upload) {
-						xhr.upload.addEventListener('progress', updateProgress, false);
-					}
-					return xhr;
-				},
 	      success: function (cc) {
 					// Reload the body after 3sec (there is 5sec waiting thing on bbscgi, I tried reloading 3sec)
 					setTimeout(function() {
+						$('form').find('input[type="submit"]').val('Posted');
 						location.reload();
 					}, 3000);
-	      }
+	      },
+	      error: function(xhr, status, er) {
+					console.log(xhr);
+					alert("Error, please check post and try again");
+					$('form').find('input[type="submit"]').val(submit_txt);
+					$('form').find('input[type="submit"]').removeAttr('disabled');
+				}
 	    });
+	    $('form').find('input[type="submit"]').val('Posting...');
+	    $('form').find('input[type="submit"]').attr('disabled', true);
 	  });
 	});
 });
