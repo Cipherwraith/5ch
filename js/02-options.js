eb230f01efc3b090aa8b09700c63bd71d7684045
lancelink <lance@bytesec.org>
2015-11-21 03:11:49 +0800

    Added `Tree View` option
    Please report bug if there is one

--- js/02-options.js
+++ js/02-options.js
@@ -1,5 +1,5 @@
 /*
- * 01-options.js - Allow users choose options for viewing threads
+ * 02-options.js - Allow users choose options for viewing threads
  *  
  * Copyright (c) 2015 Lance Link <lance@bytesec.org>
  */
@@ -9,8 +9,12 @@ $(document).ready(function() {
 	option_content    += '<div class="option_bg"></div>';
 	option_content 	  += '<div id="optionInsideView" class="option_view">';
 	option_content 	  += '<span><b><a id="close_options" href="javascript:void(0)">X</a></b></span><br><br>';
+	// Open link option
 	option_content 	  += '<input type="checkbox" name="blink_open" id="bOpen" '+ ((localStorage.backlinkOpen=="true")?'checked':'') +'>' + _("レスへのリンクを新しいページで開く") + '<br><br>';
-	option_content 	  += '<input type="checkbox" name="auto_post" id="aPost" '+ ((localStorage.autoPost=="true")?'checked':'') +'>' + _("新着レス自動表示") + '<br><br>';
+	// Tree view switch
+	option_content 	  += '<input type="checkbox" name="tree_view" id="tView" '+ ((localStorage.treeView=="true")?'checked':'') +'>' + _("ツリー表示切り替え") + '<br><br>';
+	// Auto post option
+	option_content 	  += '<input type="checkbox" name="auto_post" id="aPost" '+ ((localStorage.autoPost=="true")?'checked':'') +'>' + _("ページ遷移せずに書き込む") + '<br><br>';
 	option_content 	  += '<fieldset><legend>' + _("自動更新") + '</legend>';
 	if (localStorage.autoUpdate == "true") {
 	  option_content  += '<input type="checkbox" name="a_update" id="autoUpdate" checked>' + '   ';
@@ -21,8 +25,15 @@ $(document).ready(function() {
 	}
 	option_content    += '</fieldset>';
 	option_content    += '</div></div>';
-	$('.topmenu').append('<div id="options" class="option_text">' + _("[オプション]") + '</div>');
+	$('.topmenu').append('<div class="option_text"><a href="javascript:void(0)" id="options">' + _("[オプション]") + '</a></div>');
 	$('body').append(option_content);
+	$('.thread').css({'margin-right':'200px','word-wrap':'break-word','overflow-wrap':'break-word'});
+	$('.post').removeAttr('style').css('margin-right','15%');
+	$('.post').css('margin-right','0px');
+	$('.option_text').removeAttr('style').css('position','fixed');
+	$('.option_text').css({'position':'absolute'});
+	$('.option_view').css({'height':'250px'});
+
 	$('#options').on('click', function(e) {
 	  if (open) {
 	    $('#optionView').css('display', 'none');
@@ -38,6 +49,16 @@ $(document).ready(function() {
 			localStorage.backlinkOpen = "true";
 		} else {
 			localStorage.backlinkOpen = "false";
+		}
+	});
+	$('#tView').click(function() {
+		var $this = $(this);
+		if ($this.is(':checked')) {
+			$('.treeView').css({'display':'block'});
+			localStorage.treeView = "true";
+		} else {
+			$('.treeView').css({'display':'none'});
+			localStorage.treeView = "false";
 		}
 	});
 	$('#aPost').click(function() {
@@ -56,7 +77,7 @@ $(document).ready(function() {
 	  } else {
 	    $('#interValue').attr('disabled', true);
 	    localStorage.autoUpdate = "false";
-    	  }
+    }
 	});
 	$("#interValue").on('keyup keypress blur change', function(e) {
 		if (e.which != 8 && e.which != 0 && (e.which < 48 || e.which > 57)) {
@@ -81,6 +102,6 @@ $(document).ready(function() {
 	  if ((!container.is(e.target)) && (container.has(e.target).length == 0) && (!$('#options').is(e.target))) {
 	    $('#optionView').css('display', 'none');
 	    open = false;
-    	  }
+    }
 	});
 });
