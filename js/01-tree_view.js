8e1cf46dbc445e3cd37889b059a3c5cd44413186
lancelink <lance@bytesec.org>
2015-12-01 03:59:45 +0800

    Optimized tree_view algorithm, added support on auto_reload

--- js/01-tree_view.js
+++ js/01-tree_view.js
@@ -1,6 +1,5 @@
 /*
  * 01-tree_view.js - Show tree view triggered in 'Options'.
- *                   Hover will be disabled when tree view is turned on.
  * 
  * Copyright (c) 2015 Lance Link <lance@bytesec.org>
  */
@@ -14,37 +13,51 @@ $('document').ready(function () {
     	var reply_id = $(this).text().substring(2);
     	var post_number = $(this).parent().parent().attr('id')
     	if (tree[reply_id]) {
-    		if (tree[reply_id].indexOf("|"+post_number+"-"+reply_id+"|") < 0) {
-    			tree[reply_id] = tree[reply_id]+post_number+"-"+reply_id+"|";
+    		if (tree[reply_id].indexOf("|"+post_number+"|") < 0) {
+    			tree[reply_id] = tree[reply_id]+post_number+"|";
     		}
     	} else {
-    		tree[reply_id] = "|"+post_number+"-"+reply_id+"|";
+    		tree[reply_id] = "|"+post_number+"|";
     	}
-			for (var i = 0; i < 1000; i++) {
-				if (tree[i]) {
-	  			if(tree[i].indexOf("|"+reply_id+"-") >= 0) {
-	  				if (tree[i].indexOf("|"+post_number+"-") < 0) {
-	  					tree[i] = tree[i]+post_number+"-"+reply_id+"|";
-	  				}
-	  			}
-				}
-			}
     }
   });
   for (var i = 0; i < 1000; i++) {
   	if (tree[i]) {
 	  	var tmp = tree[i].split("|");
-	  	tmp = tmp.slice(1, -1);
-	  	var uID = (0|Math.random()*9e6).toString(36);
-	  	for (var j=0; j < tmp.length; j++) {
-	  		var fr_to = tmp[j].split("-");
-	  		var r_content = $('[data-id="'+fr_to[0]+'"]').html();
-	  		if($('#'+uID+'-'+fr_to[1]).length > 0) {
-	  			$('#'+uID+'-'+fr_to[1]).append('<div class="treeView" id="'+uID+'-'+fr_to[0]+'" style="display:none;padding-left:50px;background-color:#d9d9d9;"><div style="border-left: 1px solid #aaa;">'+r_content+'</div></div>');
-	  		} else {
-	  			$('[data-id="' + i + '"]').append('<div class="treeView" id="'+uID+'-'+fr_to[0]+'" style="display:none;padding-left:50px;background-color:#d9d9d9;"><div style="border-left: 1px solid #aaa;">'+r_content+'</div></div>');
+	  	for (var j=1; j < tmp.length-1; j++) {
+	  		var r_content = $('[data-id="'+tmp[j]+'"]').html();
+	  		$('[data-id="' + i + '"]').append('<div class="treeView tv-'+tmp[j]+'" style="display:none;padding-left:50px;background-color:#d9d9d9;"><div style="border-left: 1px solid #aaa;">'+r_content+'</div></div>');
+	  		if($('.tv-'+i).length > 0) {
+	  			$('.tv-'+i).append('<div class="treeView tv-'+tmp[j]+'" style="display:none;padding-left:50px;background-color:#d9d9d9;"><div style="border-left: 1px solid #aaa;">'+r_content+'</div></div>');
 	  		}
 	  	}
   	}
   }
+  if (localStorage.treeView == "true") {
+  	$('.treeView').css({'display':'block'});
+  }
+  $(document).on('check_tree_view', function(e, post) {
+  	var id = $(post).data('id');
+    var regExp = /\>\>[1$-9]{1,3}($|\s)/g;
+    var msg_reply = $(post).find('.message').text().match(regExp);
+    if (msg_reply) {
+      for(var ii = 0; ii<msg_reply.length; ii++) {
+        var rid = msg_reply[ii].trim().replace('>>','');
+        if (rid == id) return true;
+        if($('.tv-'+rid).length > 0) {
+          var content = $(post).not('div.treeView').clone();
+          $(content).find('.message').find('.hoverAppend').remove();
+          $(content).find('.back-links').find('.hoverAppend').remove();
+          content = content.html();
+          if (localStorage.treeView == "true") {
+          	$('[data-id="' + rid + '"]').append('<div class="treeView tv-'+id+'" style="display:block;padding-left:50px;background-color:#d9d9d9;"><div style="border-left: 1px solid #aaa;">'+content+'</div></div>');
+			  		$('.tv-'+rid).append('<div class="treeView tv-'+id+'" style="display:block;padding-left:50px;background-color:#d9d9d9;"><div style="border-left: 1px solid #aaa;">'+content+'</div></div>');
+		  		} else {
+		  			$('[data-id="' + rid + '"]').append('<div class="treeView tv-'+id+'" style="display:none;padding-left:50px;background-color:#d9d9d9;"><div style="border-left: 1px solid #aaa;">'+content+'</div></div>');
+		  			$('.tv-'+rid).append('<div class="treeView tv-'+id+'" style="display:none;padding-left:50px;background-color:#d9d9d9;"><div style="border-left: 1px solid #aaa;">'+content+'</div></div>');
+		  		}
+		  	}
+      }
+    }
+  });
 });
