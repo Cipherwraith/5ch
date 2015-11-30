59c4b81086f690b37b4dd610e1d25e1f8f9fb49b
lancelink <lance@bytesec.org>
2015-12-01 03:56:18 +0800

    Added notification trigger on new post

--- js/01-auto_reload.js
+++ js/01-auto_reload.js
@@ -1,14 +1,17 @@
 /*
  * 01-auto_reload.js - Auto reload thread on {n} time. Can be set in Options.
+ *                     Auto reload interval grow
  * 
  * Copyright (c) 2015 Lance Link <lance@bytesec.org>
  */
 $('document').ready(function () {
   if (!localStorage.autoUpdate)
-    localStorage.autoUpdate = "true";
+    localStorage.autoUpdate = "false"; // Auto reload turned off by default
   if (!localStorage.reloadInterval)
     localStorage.reloadInterval = 5;
   localStorage.reloadInterval = 5; // Always set reload interval to 5 even on refresh
+  var first_newpost_id = 0;
+  var new_post_ctr = 0;
   function reload_thread() {
     $.ajax({
       url: document.location,
@@ -16,18 +19,24 @@ $('document').ready(function () {
       cache: false,
       success: function(response) {
         var c_length = ($(response).find('div.post').length)-1;
+        new_post_ctr = 0;
+        first_newpost_id = 0;
         $(response).find('div.post').each(function(index, element) {
           var id = $(this).data('id');
           if ($('[data-id="' + id + '"]').length == 0) {
             $('.thread').append(this);
+            new_post_ctr = new_post_ctr + 1;
+            if (first_newpost_id == 0)
+              first_newpost_id = id;
             $(document).trigger('check_reply', this);
             $(document).trigger('mobile_view', this);
+            $(document).trigger('check_tree_view', this);
             if (localStorage.autoUpdate == "true")
               localStorage.reloadInterval = 5;
           } else {
             if (localStorage.autoUpdate == "true") {
               if(index == c_length) {
-                if(localStorage.reloadInterval > 79) {
+                if(localStorage.reloadInterval < 159) {
                   localStorage.reloadInterval = (localStorage.reloadInterval*2);
                 } else {
                   localStorage.reloadInterval = 5;
@@ -36,14 +45,13 @@ $('document').ready(function () {
             }
           }
         });
+        if (new_post_ctr > 0)
+          $(document).trigger('notification', '+'+new_post_ctr+'件 の新着レス <a id="s_view" onclick="s_view('+first_newpost_id+');" href="javascript:void(0)" style="text-decoration:none;color:white;">▼</a>');
+      new_post_ctr = 0;
+      first_newpost_id = 0;
       },
       error: function(xhr, status, er) {
-        $('#notific').css('display','block');
-        $('#ntxt').text(_('新しい記事を取得エラー'));
-        $('#ntxt').css('background-color','#E60000');
-        $('#notific').fadeOut(4000, function() {
-          $('#notific').css('display','none');
-        });
+        $(document).trigger('notification', '新しい記事を取得エラー');
         $('form').find('input[type="submit"]').val(_(submit_txt));
         $('form').find('input[type="submit"]').removeAttr('disabled');
       }
@@ -60,3 +68,6 @@ $('document').ready(function () {
   }
   var interval = setInterval(reload_interval, localStorage.reloadInterval);
 });
+function s_view(first_newpost_id) {
+  $("#"+first_newpost_id).get(0).scrollIntoView();
+}
